5f2f82fbc7f76df39ee9f940ac5b64649aa237da
vdeturckheim <vlad2t@hotmail.com>
2021-07-09 00:09:13 +0200

    setup build chain

--- scripts/unzip.cjs
+++ scripts/unzip.cjs
@@ -1,39 +1,17 @@
-console.log('I am supposed to unzip stuff')
-/*
 'use strict';
-import * as OS from 'os';
-import * as Fs from 'fs';
-import * as Path from 'path';
-import * as Https from 'https';
-import { default as AdmZip } from 'adm-zip';
+const AdmZip = require('adm-zip');
+const Path = require('path');
+const OS = require('os');
+const Fs = require('fs');
 
-
-const Z3_VERSION = '4.8.10';
 const FILE_TABLE = {
-    linux: 'x64-ubuntu-18.04',
-    darwin: 'x64-osx-10.15.7',
-    win32: 'x64-win'
+    linux: 'ubuntu-latest',
+    darwin: 'macos-latest',
+    win32: 'windows-latest'
 };
 
-const ZIP_NAME = `z3-${Z3_VERSION}-${FILE_TABLE[OS.platform()]}.zip`
-const URL = `https://github.com/Z3Prover/z3/releases/download/z3-${Z3_VERSION}/${ZIP_NAME}`;
-
-const download = function () {
-    const out = Fs.createWriteStream(Path.join(process.cwd(), ZIP_NAME));
-    return new Promise((resolve) => {
-        Https.get(URL, (res) => {
-            res.pipe(out);
-            res.on('end', () => {
-                return resolve(Path.join(process.cwd(), ZIP_NAME));
-            });
-        });
-    });
-};
-
-const zipPath = await download();
-Fs.mkdirSync(Path.join(process.cwd(), 'z3'));
-const zip = new AdmZip(zipPath);
-zip.extractAllTo(Path.join(process.cwd(), 'z3'), /!*overwrite*!/true);
+const zip = new AdmZip(Path.join(__dirname, `z3-${FILE_TABLE[OS.platform()]}.zip`));
+Fs.mkdirSync(Path.join(__dirname, 'z3'))
+zip.extractAllTo(Path.join(__dirname, 'z3'), true);
 
 
-*/
